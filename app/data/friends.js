4a88689fa7e7823dde70aaac112a85101a493973
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-10-08 23:20:54 -0500

    Fixed image paths

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 module.exports = [
     {
         name: "Darth Vader",
-        photo: "../../darth-vader.jpeg",
+        photo: "../../assets/images/darth-vader.jpeg",
         scores: [2, 4, 3, 5, 2, 4, 3, 2, 1, 2]
     },
     {
         name: "Fabio",
-        photo: "../../fabio.jpeg",
+        photo: "../../assets/images/fabio.jpeg",
         scores: [4, 2, 1, 3, 4, 2, 1, 3, 1, 4]
     },
     {
         name: "Elizabeth II",
-        photo: "../../elizabeth.jpeg",
+        photo: "../../assets/images/elizabeth.jpeg",
         scores: [5, 1, 2, 4, 3, 3, 3, 4, 2, 1]
     },
     {
         name: "Jeff Goldblum",
-        photo: "../../jeff-goldblum.jpeg",
+        photo: "../../assets/images/jeff-goldblum.jpeg",
         scores: [3, 2, 1, 5, 2, 1, 2, 1, 4, 5]
     },
     {
         name: "Lola the Cat",
-        photo: "../../lola.jpg",
+        photo: "../../assets/images/lola.jpg",
         scores: [4, 5, 5, 2, 5, 5, 4, 5, 3, 2]
     },
     {
         name: "Ron Burgundy",
-        photo: "../../ron-burgundy.jpeg",
+        photo: "../../assets/images/ron-burgundy.jpeg",
         scores: [1, 3, 4, 3, 1, 3, 2, 4, 2, 3]
     }
 ];
